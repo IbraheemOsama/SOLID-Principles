92a2e369d0f6eb3e74b97de57fd1075704b9efe1
Ibraheem Osama <ibraheem.osama@gmail.com>
2019-08-04 18:49:22 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# SOLID-Principles
+# SOLID Principles
+
 Repo for Solid principles arabic videos
+
+## Install dotnet core 3 (Preview at the time of the videos :) )
+
+
---
+++ SingleResponbibility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLID_Principles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Single Res!");
+        }
+    }
+}
